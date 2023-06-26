e8933c51a5b183d70da6e79eb924537e2379bfac
lbarry9 <laurabarry9@hotmail.co.uk>
2023-06-26 21:17:07 +0200

    finished

---
+++ printf/ft_puthex.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbarry <lbarry@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 00:06:07 by lbarry            #+#    #+#             */
+/*   Updated: 2023/06/23 13:12:07 by lbarry           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_puthex(unsigned long long int nbr, char c, int *len)
+{
+	char	*base;
+
+	if (c == 'x')
+		base = "0123456789abcdef";
+	else
+		base = "0123456789ABCDEF";
+	if (nbr == 0)
+	{
+		ft_putchar('0', len);
+		return ;
+	}
+	if (nbr >= 16)
+	{
+		ft_puthex(nbr / 16, c, len);
+		ft_puthex(nbr % 16, c, len);
+	}
+	if (nbr < 16)
+	{
+		write(1, &base[nbr], 1);
+		(*len)++;
+	}
+}
