944793474d123fbc681dea6aada54f1e0bd4ad59
lbarry9 <laurabarry9@hotmail.co.uk>
2024-12-13 13:54:08 +0100

    inception update

--- inception/srcs/requirements/mariadb/conf/mariadb.sh
+++ inception/srcs/requirements/mariadb/conf/mariadb.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-# explain this **********
-chsh -s $(which zsh)
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-echo "alias zshi='sh /install.sh'" >> ~/.zshrc
-
+# Start MariaDB service
 service mariadb start
 
-# make sure MariaDB is fully set up and ready for following commands
-sleep 6
+# Wait for MariaDB to be ready
+until mariadb -u root -proot1234 -e "SELECT 1" &>/dev/null; do
+  echo "Waiting for MariaDB to be ready..."
+  sleep 1
+done
 
-# MariaDB expects SQL strings on setup
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${ADMIN}\`@'localhost' IDENTIFIED BY '${ADMIN_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${DATABASE}\`.* TO \`${ADMIN}\`@'%' IDENTIFIED BY '${ADMIN_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ADMIN_PASSWORD}';"
-mariadb -e "FLUSH PRIVILEGES;"
+# Secure the initial root access
+mariadb -h localhost -u root -proot1234 <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'root1234';
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+FLUSH PRIVILEGES;
+EOF
 
-# shutdown cleanly and restarted in safe mode with desired config*
-mysqladmin -u root -p$ADMIN_PASSWORD shutdown
-mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+# Now we can proceed with your database setup using the root password
+mariadb -h localhost -u root -proot1234 <<EOF
+CREATE DATABASE IF NOT EXISTS wordpress_db;
+CREATE USER IF NOT EXISTS 'lbarry'@'localhost' IDENTIFIED BY 'db1234';
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'lbarry' IDENTIFIED BY 'db1234';
+FLUSH PRIVILEGES;
+EOF
 
-# *config:
-# listen on default port
-# accept connections from any IP address (external communication)
-# default socket file used for internal communication
-# run as mysql user- default user with low level privileges- good practice security measure
-# specify where to store databases
+# Shutdown and restart with your specified configuration
+mysqladmin -u root -proot1234 shutdown
+mysqld_safe --user=mysql --port=3306 --bind-address=0.0.0.0  --socket='/run/mysqld/mysqld.sock' --datadir='/var/lib/mysql' --pid-file='/var/run/mysqld/mysqld.pid' --skip-networking=off --max_allowed_packet=64M #--log-error='/var/log/mysql/error.log'
+
+echo "MariaDB is ready to use!"
--- inception/srcs/requirements/wordpress/conf/wordpress.sh
+++ inception/srcs/requirements/wordpress/conf/wordpress.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# downloads commandline interface for Wordpress
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# makes downloaded file executable
-chmod +x wp-cli.phar
-# move executable and make it accessible - wp command
-mv wp-cli.phar /usr/local/bin/wp
-
 # go into dir where wordpress installed, set permissions (write only by owner, read/execute by all)
 # change ownership of all files in wordpress dir to www-data (user associated with NGINX)
 cd /var/www/wordpress
@@ -16,19 +9,26 @@ chown -R www-data:www-data /var/www/wordpress
 echo "...Installing WordPress..."
 # delete any existing files in wordpress dir
 find /var/www/wordpress/ -mindepth 1 -delete
+
 # download latest version and make sure download is finished before next cmds
 wp core download --allow-root --path="/var/www/wordpress"
-sleep 6
+
+# admin or db??
+sleep 10
 
 # configures wordpress to connect to mariadb database
-wp core config --dbhost=mariadb:3306 --dbname="$DATABASE" --dbuser="$ADMIN" --dbpass="$ADMIN_PASSWORD" --allow-root --path="/var/www/wordpress"
+wp core config --dbhost=mariadb:3306 --dbname="$DATABASE" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --allow-root --path="/var/www/wordpress"
 # installs wordpress with specified site parameters
 wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$ADMIN" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --allow-root --path="/var/www/wordpress"
 # create new user as author with env varuables
 wp user create "$WP_USER" "$WP_EMAIL" --user_pass="$WP_PASSWORD" --role=author --allow-root --path="/var/www/wordpress"
 
+# ensure any cached data is cleared to avoid conflicts
+wp cache flush --allow-root
+
 # Configure PHP-FPM to run on TCP port 9000- means it can interact with the web server inside the container
 sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
 mkdir -p /run/php
+
 # start in foregrand so continues running in container
 /usr/sbin/php-fpm7.4 -F
